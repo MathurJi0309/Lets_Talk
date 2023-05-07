6b66c64ae8fe2a02844bc93b4716eb6cac58fc81
MathurJi <anileshjjssacademy84@gmail.com>
2023-05-07 16:50:56 +0530

    setup the mongoose and make the required modles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import { Button } from '@chakra-ui/react';
-import './App.css';
-import { Routes,Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import ChatPage from './Pages/ChatPage';
+import { Button } from "@chakra-ui/react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/chats" element={<ChatPage />} />
--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
   useEffect(() => {
-    const fetchChats = async() => {
-      const data = await axios.get("/api/chat");
+    const fetchChats = async () => {
+      const { data } = await axios.get("/api/chat");
       console.log(data);
+      setChats(data);
     };
     fetchChats();
   }, []);
-  return <div>ChatPage</div>;
+  return (
+    <div>
+      {chats.map((chat, index) => (
+        <>
+          <div key={index}>{chat.chatName}</div>
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
